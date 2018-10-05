89ff5febfecfde872a1c165fc8f61340294bd517
Cristian Leonardo Arango Daza <cristianarangodaza@gmail.com>
2018-10-05 17:34:05 -0500

    Demo Item compra

--- src/components/Panel/panel.jsx
+++ src/components/Panel/panel.jsx
@@ -1,9 +1,55 @@
 import React, { Component } from "react";
 
 class Panel extends Component {
-  state = {};
+  state = {
+    boletas: 5,
+    movies: [
+      {
+        id: 1,
+        name: "rápido y furioso 9",
+        boletos: 4
+      },
+      {
+        id: 2,
+        name: "max mad",
+        boletos: 4
+      }
+    ]
+  };
+  comprarBoleta = id => {
+    this.setState(prevState => {
+      const movies = [...prevState.movies];
+      let movie = movies.find(el => {
+        return el.id === id;
+      });
+      if (movie.boletos <= 0) {
+        return;
+      }
+      movie.boletos--;
+
+      return { movies: movies };
+    });
+  };
   render() {
-    return <h1>Hello</h1>;
+    const lista = this.state.movies.map(movie => (
+      <div className="card" style={{ width: "18rem" }}>
+        <h1>{movie.name}</h1>
+        <h2>Quedan {movie.boletos}</h2>
+        <button
+          className="btn btn-primary"
+          onClick={() => this.comprarBoleta(movie.id)}
+        >
+          Comprar
+        </button>
+      </div>
+    ));
+    return (
+      <div>
+        <h1>Cinema</h1>
+        <h2>Comprar boletos</h2>
+        {lista}
+      </div>
+    );
   }
 }
 
